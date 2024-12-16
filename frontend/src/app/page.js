f1ad4f0570a4c07eeb0f69b7fa21da0f10fb1d8e
liqi-tan <li.tan.2@foodpanda.com>
2024-12-16 12:23:51 +0800

    setting up frontend

---
+++ frontend/src/app/page.js
@@ -0,0 +1,11 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import Sidebar from '../components/Sidebar'
+
+export default function Home() {
+  const router = useRouter()
+
+  return (
+    <Sidebar/>
+  );
+}
