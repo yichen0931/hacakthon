c520820e85f9908b3932f1f625f7935159e94b7a
daong <daryloye@gmail.com>
2024-12-16 14:53:24 +0800

    added json tags to models

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,43 +1,43 @@
 package models
 
 type Vendor struct {
-	VendorID       string
-	VendorName     string
-	Address        string
-	IsOpen         bool
-	IsDiscountOpen bool
-	DiscountStart  int64
-	DiscountEnd    int64
+	VendorID       string `json:"VendorID"`
+	VendorName     string `json:"VendorName"`
+	Address        string `json:"Address"`
+	IsOpen         bool   `json:"IsOpen"`
+	IsDiscountOpen bool   `json:"IsDiscountOpen"`
+	DiscountStart  int64  `json:"DiscountStart"`
+	DiscountEnd    int64  `json:"DiscountEnd"`
 }
 
 type Meal struct {
-	MealID                    string
-	VendorID                  string
-	MealName                  string
-	Description               string
-	Price                     int64
-	Availability              bool
-	SustainabilityCreditScore int
+	MealID                    string `json:"MealID"`
+	VendorID                  string `json:"VendorID"`
+	MealName                  string `json:"MealName"`
+	Description               string `json:"Description"`
+	Price                     int64  `json:"Price"`
+	Availability              bool   `json:"Availability"`
+	SustainabilityCreditScore int    `json:"SustainabilityCreditScore"`
 }
 
 type Rider struct {
-	RiderID      string
-	RiderName    string
-	VehiclePlate string
-	Availability bool
+	RiderID      string `json:"RiderID"`
+	RiderName    string `json:"RiderName"`
+	VehiclePlate string `json:"VehiclePlate"`
+	Availability bool   `json:"Availability"`
 }
 
 type Customer struct {
-	CustomerID                           string
-	CustomerName                         string
-	Address                              string
-	AccumulatedSustainabilityCreditScore int
+	CustomerID                           string `json:"CustomerID"`
+	CustomerName                         string `json:"CustomerName"`
+	Address                              string `json:"Address"`
+	AccumulatedSustainabilityCreditScore int    `json:"AccumulatedSustainabilityCreditScore"`
 }
 
 type Discount struct {
-	MealID        string
-	DiscountPRice int64
-	Quantity      int
+	MealID        string `json:"MealID"`
+	DiscountPrice int64  `json:"DiscountPrice"`
+	Quantity      int    `json:"Quantity"`
 }
 
 type OrderStatus int
@@ -53,26 +53,26 @@ const (
 )
 
 type Orders struct {
-	OrderID         string
-	CustomerID      string
-	RiderID         string
-	OrderStatus     OrderStatus
-	OrderEnd        int64
-	Total           int64
-	DeliveryAddress string
+	OrderID         string      `json:"OrderID"`
+	CustomerID      string      `json:"CustomerID"`
+	RiderID         string      `json:"RiderID"`
+	OrderStatus     OrderStatus `json:"OrderStatus"`
+	OrderEnd        int64       `json:"OrderEnd"`
+	Total           int64       `json:"Total"`
+	DeliveryAddress string      `json:"DeliveryAddress"`
 }
 
 type OrderDetail struct {
-	OrderID   string
-	MealID    string
-	MealQty   int
-	MealPrice int64
+	OrderID   string `json:"OrderID"`
+	MealID    string `json:"MealID"`
+	MealQty   int    `json:"MealQty"`
+	MealPrice int64  `json:"MealPrice"`
 }
 
 type Sessions struct {
-	SessionID     string
-	UserID        string
-	SessionExpiry int64
+	SessionID     string `json:"SessionID"`
+	UserID        string `json:"UserID"`
+	SessionExpiry int64  `json:"SessionExpiry"`
 }
 
 type Role int
@@ -84,8 +84,8 @@ const (
 )
 
 type Users struct {
-	UserID   string
-	UserName string
-	Password string
-	Role     Role
+	UserID   string `json:"UserID"`
+	UserName string `json:"UserName"`
+	Password string `json:"Password"`
+	Role     Role   `json:"Role"`
 }
