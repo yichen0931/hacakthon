cdb94f7363f951950aa82962fde415f3be56fad4
yichen0931 <yichen.chiang@foodpanda.com>
2024-12-16 19:50:55 +0800

    feat: discountStatus API to let discounts launch/end by passing a JSON {discountStatus:Launch/End}

--- backend/database/database.go
+++ backend/database/database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"hackathon/models"
+	"log"
 )
 
 type DBClient struct {
@@ -18,50 +19,15 @@ func NewDBClient() *DBClient {
 		panic(err.Error())
 	}
 
-	// ping our database to check if the credentials are valid
-	//	if err := db.Ping(); err != nil {
-	//		log.Fatalf("Failed to ping database %v", err)
-	//	}
+	//ping our database to check if the credentials are valid
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping database %v", err)
+	}
 
 	fmt.Println("Successfully connected to MySQL database") // check our connection
 	newDB := &DBClient{DB: db}
 	return newDB
 }
-
-//func (db *DBClient) VendorGetMeals() {
-//	results, err := db.DB.Query("SELECT * FROM Meal")
-//
-//	if err != nil {
-//		panic(err.Error())
-//	}
-//
-//	defer results.Close()
-//
-//	for results.Next() {
-//		var meal models.Meal
-//		err = results.Scan(
-//			&meal.MealID,
-//			&meal.MealName,
-//			&meal.Description,
-//			&meal.Price)
-//
-//		if err != nil {
-//			panic(err.Error())
-//		}
-//
-//		fmt.Printf("Meals successfully retrieved %s %s %s %d\n", meal.MealID, meal.MealName, meal.Description, meal.Price)
-//	}
-//}
-
-//func (db *DBClient) SetDiscountTime(startTime string, endTime string) {
-//	query := fmt.Sprintf("UPDATE Vendor SET DiscountStart = '%s', DiscountEnd = '%s'", startTime, endTime)
-//
-//	if _, err := db.DB.Exec(query); err != nil {
-//		log.Fatalf("Failed to update Vendor Discount time %v", err)
-//		return
-//	}
-//	log.Printf("Successfully updated Vendor Discount time to start at '%s', and end at '%s'", startTime, endTime)
-//}
 
 func (db *DBClient) DiscountStatus(res string) error {
 	status := models.Vendor{}
@@ -75,11 +41,3 @@ func (db *DBClient) DiscountStatus(res string) error {
 	}
 	return fmt.Errorf("Invalid status %v", res)
 }
-
-//func (db *DBClient) SetDiscountedPrice(status *models.Vendor, meal *models.Meal) {
-//	if status.IsDiscountOpen == true {
-//
-//	} else if status.IsDiscountOpen == false || status.DiscountStart != time.Now().Format("2006-01-02 15:04:05") {
-//		log.Fatalln("Discount ")
-//	}
-//}
