48259babc8ed09546c04fa205ca7ccea15c38515
musfirahwani <musfirah.wani@foodpanda.com>
2024-12-16 15:34:29 +0800

    added go mod file for uuid

--- backend/server/server.go
+++ backend/server/server.go
@@ -33,8 +33,27 @@ func (a *Apiserver) VendorDiscount(w http.ResponseWriter, r *http.Request) {
 func (a *Apiserver) GetCustomerDiscount(w http.ResponseWriter, r *http.Request) {
 
 }
-func (a *Apiserver) GetCustomerDiscountIndividual(w http.ResponseWriter, r *http.Request) {
+func (a *Apiserver) GetCustomerDiscountIndividual(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet { //GET ALL Meals from Vendors
+		//getVendorID := req.URL.Path
+		//
+		//parts := strings.Split(getVendorID, "/")
+		//var vendorID string
 
+	}
+	//getVendor := r.URL.Path
+	//urlPath := req.URL.Path //// Get the full URL path. attendance/<id>
+	//
+	//parts := strings.Split(urlPath, "/") // Split the URL path by "/"
+	//var attendanceID string
+	//
+	//if len(parts) > 1 { // Assuming the ID is the last part of the URL path
+	//	attendanceID = parts[len(parts)-1]
+	//} else {
+	//	logrus.Info("Attendance ID not provided")
+	//	http.Error(res, "Attendance ID not provided", http.StatusBadRequest)
+	//	return
+	//}
 }
 
 func (a *Apiserver) Checkout(w http.ResponseWriter, r *http.Request) {
