0da268a8010fc963173cc2f06757cc611940af07
anisha-sls <anisha.singh@foodpanda.com>
2024-12-16 15:34:44 +0800

    update sidebar add current selector to change which item in the sidebar is highlighted

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,6 +6,6 @@ export default function Home() {
   const router = useRouter()
 
   return (
-    <Sidebar/>
+    <Sidebar current="Dashboard"/>
   );
 }
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,15 +2,15 @@ import { useRouter } from 'next/navigation'
 import Image from "next/image"
 import logo from '../assets/foodpanda-app-icon-square.png';
 
-// interface Props {
-//   name
-
-// }
-
+// To render each item based
+// Properties : name, link (optional)
+// Accept two properties name and link
+// Name is displayed as text and the link is the link that user will be sent to when pressed
+// If no link is provided link is default to "#"
 function Item(props) {
   const router = useRouter()
   let cssSelectors = "p-2 mt-2 flex items-center px-4 duration-300 cursor-pointer hover:bg-pink-500 hover:text-white "
-  if (props.selected == "true") {
+  if (props.selected) {
     cssSelectors += "text-pink-500 font-bold border-l-4 border-solid border-pink-500"
   } else {
     cssSelectors += "text-black"    
@@ -26,36 +26,44 @@ function Item(props) {
   )
 }
 
+// Returns Sidebar. 
+// Properties : current
+// When calling sidebar, use current property to select which item to be selected. i.e. current="Dashboard"
+// That item will have a pink text and a left border to indicate it is selected to the user
+// To add or edit the name/link for item, change it in the const items variable
 function Sidebar(props) {
+  const items = [
+    {name:"Dashboard", link:"#"},
+    {name:"Marketing", link:"#"},
+    {name:"Orders", link:"#"},
+    {name:"Rating and Reviews", link:"#"},
+    {name:"Reports", link:"#"},
+    {name:"Concepts", link:"#"},
+    {name:"Notification Centre", link:"#"},
+    {name:"Menu Management", link:"#"},
+    {name:"Opening Times", link:"#"},
+    {name:"Invoices", link:"#"},
+    {name:"University", link:"#"},
+    {name:"Discounts", link:"./discounts"},
+
+  ]
   return (
       <div>
         <span className="absolute text-black text-4xl top-5 left-4 cursor-pointer">
           <i className="bi bi-filter-left px-2 bg-white-900 rounded-md"></i>
         </span>
-        <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-white-900">
+        <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-white-900 border-r-2 border-solid">
           <div className="text-black-100 text-xl">
-              <div className="p-2.5 mt-1 flex items-center">
-              <Image src={logo} alt="Logo" width={200} height={100}/>
-                {/* <img className="logo" src={pandaLogo}/> */}
-                {/* <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
-                <h1 className="font-bold text-black-200 text-[15px] ml-3">TailwindCSS</h1>
-                <i className="bi bi-x cursor-pointer ml-28 lg:hidden"></i> */}
+              <div className="p-3 flex items-start justify-start">
+              <Image src={logo} alt="Logo" width="auto" height={50}/>
               </div>
               <div className="my-2 bg-white-600 h-[1px]"></div>
           </div>
 
-          <Item name="Dashboard" selected="true"/>
-          <Item name="Marketing"/>
-          <Item name="Orders"/>
-          <Item name="Rating and Reviews"/>
-          <Item name="Reports"/>
-          <Item name="Concepts"/>
-          <Item name="Notification Centre"/>
-          <Item name="Menu Management"/>
-          <Item name="Opening Times"/>
-          <Item name="Invoices"/>
-          <Item name="University"/>
-          <Item name="Discounts" link="./discounts"/>
+          {items.map((item) => (
+            <Item name={item.name} link={item.link} selected={props.current == item.name && true} key={item.name}/>
+          ))}
+          <Item/>
 
         </div>
       </div>
